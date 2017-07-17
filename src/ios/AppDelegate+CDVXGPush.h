8a9b74628d89fddcee604ee2ea255530b204587d
Cao Jie <jacky_cao@itsnow.com>
2017-07-17 18:03:19 +0800

    add ios notification

--- src/ios/AppDelegate+CDVXGPush.h
+++ src/ios/AppDelegate+CDVXGPush.h
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
+#import <Foundation/Foundation.h>
 
 @interface AppDelegate (CDVXGPush)
-
 @end
--- src/ios/CDVXGPushPlugin.h
+++ src/ios/CDVXGPushPlugin.h
@@ -1,51 +1,18 @@
+#import "CDVXGPushUtil.h"
 #import <Cordova/CDV.h>
 #import <Foundation/Foundation.h>
 
-#import "XGPush.h"
-#import "XGSetting.h"
+@interface CDVXGPushPlugin: CDVPlugin
 
-#define kXGPushPluginReceiveNotification @"XGPushPluginReceiveNofication"
+- (void) didRegisterForRemoteNotificationsWithDeviceToken:(NSNotification*)notification;
+- (void) didFailToRegisterForRemoteNotificationsWithError:(NSNotification*)notification;
+- (void) didReceiveRemoteNotification:(NSNotification*)notification;
 
-typedef void (^CallbackBlock) (void);
+- (void) registerpush:(CDVInvokedUrlCommand*)command;
+- (void) unregisterpush:(CDVInvokedUrlCommand*)command;
+- (void) addlistener:(CDVInvokedUrlCommand*)command;
 
-@interface CDVXGPushPlugin: CDVPlugin{
-}
-
-+(void)setLaunchOptions:(NSDictionary *)theLaunchOptions;
-
-/*
- notification
- */
-- (void) registerNotificationForIOS7;
-- (void) registerNotificationForIOS8;
-- (void) registerNotification;
-
-/*
- plugin
- */
-- (void) startApp:(uint32_t)assessId key:(NSString*) accessKey;
-
-- (void) didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken;
-- (void) didFailToRegisterForRemoteNotificationsWithError:(NSError*)err;
-- (void) didReceiveRemoteNotification:(NSDictionary*)userInfo;
-
-- (void) sendMessage:(NSString*) type data:(NSDictionary*)dict;
-
-- (void) addListener:(CDVInvokedUrlCommand*)command;
-- (void) registerPush:(CDVInvokedUrlCommand*)command;
-- (void) unRegisterPush:(CDVInvokedUrlCommand*)command;
-- (void) getLaunchInfo:(CDVInvokedUrlCommand*)command;
-
-- (void) setTag:(CDVInvokedUrlCommand*)command;
-- (void) deleteTag:(CDVInvokedUrlCommand*)command;
-
-- (void) addLocalNotification:(CDVInvokedUrlCommand*)command;
-
-- (void) enableDebug:(CDVInvokedUrlCommand*)command;
-- (void) getToken:(CDVInvokedUrlCommand*)command;
-- (void) setAccessInfo:(CDVInvokedUrlCommand*)command;
-
-@property NSData* deviceToken;
-@property (nonatomic, copy) NSString* callbackId;
+@property CDVXGPushUtil* util;
+@property NSMutableArray* callbackIds;
 
 @end
